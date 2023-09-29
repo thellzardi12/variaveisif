7b180f6d47c127f10351b2d9d7bed784442fb2fb
thellzardi12 <130668477+thellzardi12@users.noreply.github.com>
2023-09-29 10:04:48 -0300

    commit thell

---
+++ exercicio.php
@@ -0,0 +1,19 @@
+<?php
+
+//declaração de variáveis
+$num1 = 44;
+$num2 = 6;
+
+//declaração de pessoa
+$pes1 = "Thell";
+
+//identifica se a variável é um numero
+if (is_int($num1)){
+    $num3 = $num1 * 2;
+} if ($num3 > 100){
+    echo "é maior de 100";
+} else {
+    echo "Não é maior que 100";
+}
+
+?>
